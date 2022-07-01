f1a14c1e8247587ae26fe9d2dbd7fcaac4029470
Sahil Shekh <95908470+sahilshekh@users.noreply.github.com>
2022-07-01 23:58:27 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+// import "./styles.css";
+import "./App.css"
+import Form from "./components/Form.jsx";
+
+export default function App() {
+  const { isRunning, start, stop, seconds } = Form(5);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>useTimer custom hook</h1>
+      <h2>{isRunning ? seconds : "No Timer Running"}</h2>
+      <button onClick={start} disabled={isRunning}>
+        Start Timer
+      </button>
+      <button onClick={stop} disabled={!isRunning}>
+        Stop Timer
+      </button>
     </div>
   );
 }
-
-export default App;
